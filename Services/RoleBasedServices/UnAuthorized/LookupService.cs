e5ab6833e3c581a1f3e9628923ddfc42de4fac99
Sweety-deeps <deepszweety5@gmail.com>
2024-10-22 22:20:28 +0530

    Create LookupService.cs

---
+++ Services/RoleBasedServices/UnAuthorized/LookupService.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Models;
+using Domain.Models.Users;
+using Services.Abstractions;
+
+namespace Services.UnAuthorized
+{
+    public class LookupService : ILookupService
+    {
+        public bool CanAdd(LoggedInUser user)
+        {
+            return false;
+        }
+
+        public bool CanDelete(LoggedInUser user)
+        {
+            return false;
+        }
+
+        public bool CanEdit(LoggedInUser user)
+        {
+            return false;
+        }
+
+        public bool CanView(LoggedInUser user)
+        {
+            return false;
+        }
+        
+        public List<LookupValue> GetLookupFields(LoggedInUser user, int id, string? value)
+        {
+            throw new UnauthorizedAccessException();
+        }
+    }
+}
